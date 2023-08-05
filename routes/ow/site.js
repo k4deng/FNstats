3c5631589979eea6933c7b7bc2969beaad630ee0
k4deng <70725766+k4deng@users.noreply.github.com>
2023-08-05 17:25:28 -0400

    dont use .then anymore

--- routes/ow/site.js
+++ routes/ow/site.js
@@ -28,56 +28,54 @@ module.exports = function(client, dataDir, templateDir) {
     res.redirect(`${client.url}/ow/user/${encodeURIComponent(req.query.user)}${req.query?.platform ? "?platform="+req.query.platform : ""}`)
   });
 
-  router.get("/user/:user", (req, res) => {
+  router.get("/user/:user", async (req, res) => {
 
-    fnapi.getStats(req.params.user, req.query?.platform, req.query?.accounts)
-      .then(function(stats) {
+    const stats = await fnapi.getStats(req.params.user, req.query?.platform, req.query?.accounts)
 
-        // found stats
-        if (stats.status == 1) {
-          if (req.params.user !== stats.result.username) res.redirect(`${encodeURIComponent(stats.result.username)}${req.query?.platform ? "?platform="+req.query.platform : ""}${req.query?.fromEvent ? "?fromEvent="+req.query.fromEvent : ""}`)
-          else res.render(
-            path.resolve(`${dataDir}${path.sep}views${path.sep}ow${path.sep}user.ejs`), {
-              site: client,
-              stats: stats.result,
-              moment: moment,
-              fromEvent: req.query?.fromEvent
-            });
-        }
+    // found stats
+    if (stats.status == 1) {
+      if (req.params.user !== stats.result.username) res.redirect(`${encodeURIComponent(stats.result.username)}${req.query?.platform ? "?platform="+req.query.platform : ""}${req.query?.fromEvent ? "?fromEvent="+req.query.fromEvent : ""}`)
+      else res.render(
+        path.resolve(`${dataDir}${path.sep}views${path.sep}ow${path.sep}user.ejs`), {
+          site: client,
+          stats: stats.result,
+          moment: moment,
+          fromEvent: req.query?.fromEvent
+        });
+    }
 
-        // hasnt played that platform
-        if (stats.status == 11) res.render(
-          path.resolve(`${dataDir}${path.sep}views${path.sep}ow${path.sep}user-error.ejs`), {
-            site: client,
-            stats: stats.result
-          });
-
-        // private
-        if (stats.status == 2) res.render(
-          path.resolve(`${dataDir}${path.sep}views${path.sep}ow${path.sep}user-error.ejs`), {
-            site: client,
-            stats: stats.result
-          });
-
-        // no user found
-        if (stats.status == 3) res.status(404).render(
-          path.resolve(`${templateDir}${path.sep}ow${path.sep}error.ejs`), {
-            site: client,
-            errorNum: "404",
-            errorDesc: "User Not Found",
-            errorSlug: "It looks like that user doesnt exist..."
-          });
-
-        // error finding stats
-        if (stats.status == 5) res.status(500).render(
-          path.resolve(`${templateDir}${path.sep}ow${path.sep}error.ejs`), {
-            site: client,
-            errorNum: "500",
-            errorDesc: "Error Fetching Stats",
-            errorSlug: "You should probably let an admin know..."
-          });
-
+    // hasnt played that platform
+    if (stats.status == 11) res.render(
+      path.resolve(`${dataDir}${path.sep}views${path.sep}ow${path.sep}user-error.ejs`), {
+        site: client,
+        stats: stats.result
       });
+
+    // private
+    if (stats.status == 2) res.render(
+      path.resolve(`${dataDir}${path.sep}views${path.sep}ow${path.sep}user-error.ejs`), {
+        site: client,
+        stats: stats.result
+      });
+
+    // no user found
+    if (stats.status == 3) res.status(404).render(
+      path.resolve(`${templateDir}${path.sep}ow${path.sep}error.ejs`), {
+        site: client,
+        errorNum: "404",
+        errorDesc: "User Not Found",
+        errorSlug: "It looks like that user doesnt exist..."
+      });
+
+    // error finding stats
+    if (stats.status == 5) res.status(500).render(
+      path.resolve(`${templateDir}${path.sep}ow${path.sep}error.ejs`), {
+        site: client,
+        errorNum: "500",
+        errorDesc: "Error Fetching Stats",
+        errorSlug: "You should probably let an admin know..."
+      });
+
   });
 
   // 404 errors
--- routes/user.js
+++ routes/user.js
@@ -17,54 +17,51 @@ const router = express.Router();
 
 module.exports = function(client, dataDir, templateDir) {
 
-  router.get("/:user", (req, res) => {
+  router.get("/:user", async (req, res) => {
 
-    fnapi.getStats(req.params.user, req.query?.platform, req.query?.accounts)
-      .then(function(stats) {
+    const stats = await fnapi.getStats(req.params.user, req.query?.platform, req.query?.accounts);
 
-        // found stats
-        if (stats.status == 1) {
-          if (req.params.user !== stats.result.username) res.redirect(`${encodeURIComponent(stats.result.username)}${req.query?.platform ? "?platform="+req.query.platform : ""}`)
-          else res.render(
-            path.resolve(`${dataDir}${path.sep}views${path.sep}user${path.sep}user.ejs`), {
-              site: client,
-              stats: stats.result,
-              moment: moment
-            });
-        }
+    // found stats
+    if (stats.status == 1) {
+      if (req.params.user !== stats.result.username) res.redirect(`${encodeURIComponent(stats.result.username)}${req.query?.platform ? "?platform="+req.query.platform : ""}`)
+      else res.render(
+        path.resolve(`${dataDir}${path.sep}views${path.sep}user${path.sep}user.ejs`), {
+          site: client,
+          stats: stats.result,
+          moment: moment
+        });
+    }
 
-        // hasnt played that platform
-        if (stats.status == 11) res.render(
-          path.resolve(`${dataDir}${path.sep}views${path.sep}user${path.sep}error.ejs`), {
-            site: client,
-            stats: stats.result
-          });
+    // hasn't played that platform
+    if (stats.status == 11) res.render(
+      path.resolve(`${dataDir}${path.sep}views${path.sep}user${path.sep}error.ejs`), {
+        site: client,
+        stats: stats.result
+      });
 
-        // private
-        if (stats.status == 2) res.render(
-          path.resolve(`${dataDir}${path.sep}views${path.sep}user${path.sep}error.ejs`), {
-            site: client,
-            stats: stats.result
-          });
+    // private
+    if (stats.status == 2) res.render(
+      path.resolve(`${dataDir}${path.sep}views${path.sep}user${path.sep}error.ejs`), {
+        site: client,
+        stats: stats.result
+      });
 
-        // no user found
-        if (stats.status == 3) res.status(404).render(
-          path.resolve(`${templateDir}${path.sep}error.ejs`), {
-            site: client,
-            errorNum: "404",
-            errorDesc: "User Not Found",
-            errorSlug: "It looks like that user doesnt exist..."
-          });
+    // no user found
+    if (stats.status == 3) res.status(404).render(
+      path.resolve(`${templateDir}${path.sep}error.ejs`), {
+        site: client,
+        errorNum: "404",
+        errorDesc: "User Not Found",
+        errorSlug: "It looks like that user doesnt exist..."
+      });
 
-        // error finding stats
-        if (stats.status == 5) res.status(500).render(
-          path.resolve(`${templateDir}${path.sep}error.ejs`), {
-            site: client,
-            errorNum: "500",
-            errorDesc: "Error Fetching Stats",
-            errorSlug: "You should probably let an admin know..."
-          });
-
+    // error finding stats
+    if (stats.status == 5) res.status(500).render(
+      path.resolve(`${templateDir}${path.sep}error.ejs`), {
+        site: client,
+        errorNum: "500",
+        errorDesc: "Error Fetching Stats",
+        errorSlug: "You should probably let an admin know..."
       });
     
   });
